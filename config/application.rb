8b834be7e6c2390a571aa39ac2106c6fd0bf810a
yasu <wing101048@gmail.com>
2022-03-28 21:28:29 +0900

    postsコントローラとビューファイルを作成

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module PostBootstrapApp
       g.skip_routes true
       g.assets false
       g.helper false
-      
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
