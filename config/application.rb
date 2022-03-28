0afaddde8c1cf54151572d54ff68fd58fe278109
yasu <wing101048@gmail.com>
2022-03-28 21:24:28 +0900

    rails g controllerコマンドで作成されるファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,12 @@ module PostBootstrapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
